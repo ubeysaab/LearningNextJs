c8f170e7d8771f9898f65370fb782b7c6bdc8ded
ubeysaab <aboosh.sa262000@gmail.com>
2025-07-21 22:27:29 +0300

    :bug: fix bug that causes error when deploying to the prduction environment

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
-import { fetchCardData, fetchLatestInvoices } from '../lib/data';
+
 
 export default async function Page() {
 
